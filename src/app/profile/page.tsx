bdf3cc476920d08423322b191b9484b7323a578e
Mohamed Amr <M.Amr@ipic-eg.net>
2025-12-23 08:54:34 +0200

    resolve some bugs

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,10 @@
 import { Button, Container, Paper, TextField } from '@mui/material'
 import axios from 'axios'
 import React from 'react'
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { styled } from '@mui/material/styles';
-import toast from 'react-hot-toast';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import { styled } from '@mui/material/styles'
+import toast from 'react-hot-toast'
+
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
   clipPath: 'inset(50%)',
@@ -15,77 +16,66 @@ const VisuallyHiddenInput = styled('input')({
   left: 0,
   whiteSpace: 'nowrap',
   width: 1,
-});
+})
+
 const Profile = () => {
-  async function addPost(values){
-    const {data} = await axios.post('https://linked-posts.routemisr.com/posts',values,{
-      headers:{
-        token:localStorage.getItem('token')
+
+  async function addPost(values: FormData): Promise<void> {
+    await axios.post(
+      'https://linked-posts.routemisr.com/posts',
+      values,
+      {
+        headers: {
+          token: localStorage.getItem('token') || ''
+        }
       }
-    })
+    )
     toast.success('Posted Successfully')
-    console.log(data)
   }
-  function handleSubmit(e:Event){
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    const body = e.target?.body.value
-    const image =e.target?.image.files[0]
 
-    const formData = new FormData()
-    formData.append('body',body)
-    formData.append('image',image)
+    const formData = new FormData(e.currentTarget)
     addPost(formData)
-
   }
+
   return (
     <Container>
-    <Paper elevation={10} sx={{ m: 3, p: 3 }}>
-      <form
-        onSubmit={handleSubmit}
-        style={{ display: "flex", flexDirection: "column", gap: "20px" }}
-      >
-        <h2>Post Now</h2>
-        <TextField
-          // name='body'
-          id="body"
-          // label="body"
-          type="text"
-          multiline
-          rows={4}
-          // variant="standard"
-          fullWidth
-          defaultValue={'default Value'}
-        />
-        {/* <TextField
-          name='image'
-          id="image"
-          label="image"
-          type="file"
-          variant="standard"
-          fullWidth
-        /> */}
+      <Paper elevation={10} sx={{ m: 3, p: 3 }}>
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}
+        >
+          <h2>Post Now</h2>
+
+          <TextField
+            name="body"
+            id="body"
+            multiline
+            rows={4}
+            fullWidth
+            defaultValue="default Value"
+          />
+
           <Button
-              component="label"
-              role={undefined}
-              variant="outlined"
-              tabIndex={-1}
-              startIcon={<CloudUploadIcon />}
-            >
-              Upload files
-              <VisuallyHiddenInput
-                type="file"
-                onChange={(event) => console.log(event.target.files)}
-                id='image'
-              />
+            component="label"
+            variant="outlined"
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload image
+            <VisuallyHiddenInput
+              type="file"
+              name="image"
+            />
           </Button>
-       
+
           <Button type="submit" variant="contained">
             Post
           </Button>
-       
-      </form>
-    </Paper>
-  </Container>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
